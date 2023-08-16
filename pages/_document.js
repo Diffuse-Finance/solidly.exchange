f366e953f3a17f5a9323cb11b6429b541576595b
matiash-ok <mathy0095@gmail.com>
2023-08-16 17:18:20 -0300

    feature styled header and remove background image

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,9 @@ export default class MyDocument extends Document {
           <link rel="stylesheet" href="/fonts/Inter/Inter.css" />
           <link rel="stylesheet" href="/fonts/MonumentExt/Monument.css" />
           <link rel="stylesheet" href="/fonts/Druk/Druk.css" />
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=JetBrains+Mono:wght@100;500&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
